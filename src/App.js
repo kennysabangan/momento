3c05e64769f8cb318af37e2072f6b878765c707a
kennysabangan <61653677+kennysabangan@users.noreply.github.com>
2022-04-08 14:27:57 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Card from './components/Card';
+import shuffle from './utilities/shuffle';
 
 function App() {
+  const [cards, setCards] = useState(shuffle);
+  const [pickOne, setPickOne] = useState(null); // First selection
+  const [pickTwo, setPickTwo] = useState(null); // Second selection
+  const [disabled, setDisabled] = useState(false); // Delay handler
+  const [wins, setWins] = useState(0); // Win streak
+
+  const handleClick = (card) => {
+    if (!disabled) {
+      pickOne ? setPickTwo(card): setPickOne(card);
+    }
+  }
+
+  const handleTurn = () => {
+    setPickOne(null);
+    setPickTwo(null);
+    setDisabled(false);
+  }
+
+  useEffect(() => {
+    let pickTimer;
+
+    if (pickOne && pickTwo) {
+      // Check if the cards the same
+      if (pickOne.image === pickTwo.image) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.image === pickOne.image) {
+              return { ...card, matched: true }
+            } else {
+              // No match
+              return card;
+            }
+          })
+        })
+        handleTurn();
+      } else {
+        setDisabled(true);
+
+        pickTimer = setTimeout(() => {
+          handleTurn();
+        }, 1000);
+      }
+    }
+
+    return () => {
+      clearTimeout(pickTimer);
+    }
+
+  }, [cards, pickOne, pickTwo])
+
+  // If player found all matches, handle accordingly
+  useEffect(() => {
+    // Check for any remaining cards matches
+    const checkWin = cards.filter((card) => !card.matched);
+
+    // All matches made, handle win/badge counters
+    if (cards.length && checkWin.length < 1) {
+      console.log('You win!');
+      setWins(wins + 1);
+      handleTurn();
+      setCards(shuffle);
+    }
+  }, [cards, wins])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+
+          return (
+            <Card
+              key={id}
+              image={image}
+              selected={card === pickOne || card === pickTwo || matched}
+              onClick={() => handleClick(card)}
+            />
+          )
+        })}
+      </div>
+
+    </>
   );
 }
 
